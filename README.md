d213126931745a1ba61b09f766f20cddaebeb216
Theethawat Savastham <5910110150@psu.ac.th>
2020-12-14 00:21:18 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,70 +1,12 @@
-# Getting Started with Create React App
+# TDC COVID-19 Monitor
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Tin and TheDuckCreator COVID-19 Monitor From Thailand OpenData API [Data.go.th](https://opend.data.go.th) and Covid Stat thailand https://covid19.ddc.moph.go.th/th/api Using Node.js, React
 
-## Available Scripts
+### Objective
 
-In the project directory, you can run:
+- Easy View any and visualize case detail,Because of today COVID-19 stat page from DDC MOPH shown above although the daily case is up-to-date but the case detail provide visualized data only data from 01-01-2020 to 07-09-2020.
+- Data from OpenData (Data.go.th) is active and that APIs can make it more useful.
 
-### `npm start`
+### Problem Now
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+- Stuck with CORS Policy of Data.go.th Server
--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'antd/dist/antd.css'
+import { Layout, Avatar, Card, Row, Col } from 'antd'
+import axios from 'axios'
+import './App.css'
+import Text from 'antd/lib/typography/Text'
+const { Header, Content, Footer } = Layout
 
 function App() {
+  const [dailyCase, setdailyCase] = useState(null)
+  const [latestActiveCase, setlatestActiveCase] = useState(null)
+
+  useEffect(() => {
+    axios.get('https://covid19.th-stat.com/api/open/today').then((res) => {
+      console.log(res.data)
+      setdailyCase(res.data)
+    })
+    return () => {}
+  }, [])
+
+  useEffect(() => {
+    var config = {
+      method: 'get',
+      url:
+        'https://opend.data.go.th/get-ckan/datastore_search_sql?sql=SELECT * from "24ac8406-0cf9-4f8e-a55e-b53cf6766d1a" ORDER BY no DESC LIMIT 20',
+      headers: {
+        'api-key': process.env.REACT_APP_API_KEY,
+        'X-Requested-With': 'XMLHttpRequest',
+        'Access-Control-Allow-Origin': '*',
+        withCredentials: true,
+      },
+    }
+
+    axios(config)
+      .then(function (res) {
+        console.log(res.data)
+        setlatestActiveCase(res.data)
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+
+    return () => {}
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>
+        <div className='d-flex pt-3 kanit'>
+          <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>
+            TC
+          </Avatar>
+          <h3 className='text-white ml-2 '>TDC COVID-19 </h3>
+        </div>
+      </Header>
+
+      <Content style={{ padding: '50px 50px' }}>
+        <div className='site-layout-content'>
+          <Card title='ข้อมูลสถานะ COVID-19 รายวัน' className='kanit'>
+            {dailyCase && (
+              <>
+                <h3>
+                  {' '}
+                  {dailyCase.Confirmed} รายการ{' '}
+                  <Text type='warning'> (+ {dailyCase.NewConfirmed} ราย) </Text>{' '}
+                </h3>
+                <Row>
+                  <Col span={8}>
+                    <h5>เสียชีวิต</h5>
+                    <h5>
+                      {' '}
+                      {dailyCase.Deaths} ราย{' '}
+                      <span className='text-danger'>
+                        {' '}
+                        + {dailyCase.NewDeaths} ราย
+                      </span>
+                    </h5>
+                  </Col>
+                  <Col span={8}>
+                    <h5>รักษาอยู่</h5>
+                    <h5>
+                      {' '}
+                      {dailyCase.Hospitalized} ราย{' '}
+                      <span className='text-success'>
+                        {' '}
+                        + {dailyCase.NewHospitalized} ราย
+                      </span>
+                    </h5>
+                  </Col>
+                  <Col span={8}>
+                    <h5>รักษาหาย</h5>
+                    <h5>
+                      {' '}
+                      {dailyCase.Recovered} ราย{' '}
+                      <span className='text-info'>
+                        {' '}
+                        + {dailyCase.NewRecovered} ราย
+                      </span>
+                    </h5>
+                  </Col>
+                </Row>
+                <br />
+                <h6 className='text-muted'>
+                  {' '}
+                  ข้อมูล ณ วันที่ {dailyCase.UpdateDate}{' '}
+                </h6>
+              </>
+            )}
+          </Card>
+
+          <br />
+          <Card title='ข้อมูลสถานะ 50 Case ล่าสุด' className='kanit'></Card>
+        </div>
+      </Content>
+
+      <Footer style={{ textAlign: 'center' }}>
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
